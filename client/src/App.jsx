4d84b7cc4853371a78d4973a5a4264495b1ed70f
modibo keita <93909081keita@gmail.com>
2025-02-23 21:21:59 +0200

    Routes configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,28 @@
 
 import './App.css'
 
+import Layout from './components/Layout'
+import About from './pages/About'
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom"
+
+
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route path="/about" element={<About />} />
+  </Route>
+))
+
+
 function App() {
 
-
   return (
-    <>
-      <div>
-        <h1>Hello</h1>
-      </div>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
